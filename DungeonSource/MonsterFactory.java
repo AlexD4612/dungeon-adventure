a34c84cbb78104e6058fa5163de35f9ec3684caf
Graham Mix <gmix@eagles.ewu.edu>
2020-03-04 09:12:14 -0800

    Created the Monster/Hero Factory and createMonster/Hero methods

--- DungeonSource/MonsterFactory.java
+++ DungeonSource/MonsterFactory.java
@@ -1,1 +1,19 @@
-// Empty file, to be created later
+public class MonsterFactory {
+	
+	public Monster createMonster(int monsterNum) {
+		switch(monsterNum) {
+			case 1:
+				return new Gremlin();
+				break;
+			case 2:
+				return new Ogre();
+				break;
+			case 3:
+				return new Skeleton();
+				break;
+			case default:
+				return new Skeleton();
+				break;
+		}
+	}
+}
