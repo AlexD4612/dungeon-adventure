e32a02c6520c147b05c40de611fc95bf87bcbd09
Graham Mix <gmix@eagles.ewu.edu>
2020-03-04 10:44:25 -0800

    Updated Hero/Monster Factory

--- DungeonSource/HeroFactory.java
+++ DungeonSource/HeroFactory.java
@@ -6,16 +6,11 @@ public class HeroFactory {
 		switch(heroNum) {
 			case 1:
 				return new Warrior();
-<<<<<<< HEAD
 			case 2:
 				return new Sorceress();
 			case 3:
 				return new Thief();
-			case default:
-=======
-				break;
 			default:
->>>>>>> branch 'Graham' of https://github.com/AlexD4612/dungeon-adventure.git
 				return new Warrior();
 		}
 	}
--- DungeonSource/MonsterFactory.java
+++ DungeonSource/MonsterFactory.java
@@ -10,12 +10,7 @@ public class MonsterFactory {
 				return new Ogre();
 			case 3:
 				return new Skeleton();
-<<<<<<< HEAD
-			case default:
-=======
-				break;
 			default:
->>>>>>> branch 'Graham' of https://github.com/AlexD4612/dungeon-adventure.git
 				return new Skeleton();
 		}
 	}
