cfc7beebd28c963efaae298ae7b5c8f80df3fc16
Graham Mix <gmix@eagles.ewu.edu>
2020-03-04 09:20:58 -0800

    Filled out Hero/MonsterFactory

--- DungeonSource/HeroFactory.java
+++ DungeonSource/HeroFactory.java
@@ -1,1 +1,19 @@
-// Empty file, to be created later
+public class HeorFactory {
+	
+	public Hero createHero(int heroNum) {
+		switch(heroNum) {
+			case 1:
+				return new Sorceress();
+				break;
+			case 2:
+				return new Thief();
+				break;
+			case 3:
+				return new Warrior();
+				break;
+			case default:
+				return new Warrior();
+				break;
+		}
+	}
+}
